c28fc47da13a7f0993ff9becd901ca6768e578a6
Clare Jackson <clare-jackson@lambdastudents.com>
2021-02-11 10:33:11 -0500

    Hello World working.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,51 @@
 // BUILD YOUR SERVER HERE
+const express = require("express")
+const db = require("./users/model")
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.json({ message: "Hello, World" })
+})
+
+//POST 
+server.post("/api/users", (req, res) => {
+  const newUser = db.insert({
+    name: req.body.name,
+    bio: req.body.bio
+  })
+  if (!req.body.name || !req.body.bio){
+    res.status(400).json({
+      message: "Please provide name and bio for the user",
+    })
+  } else if (!newUser){
+    res.status(500).json({
+      message: "There was an error while saving the user to the database"
+    })
+  } else {
+    res.status(201).json(newUser)
+  }
+})
+
+//GET
+server.get("/api/users", (req, res) => {
+  const users = db.find()
+  if (users) {
+    res.json(users)
+  } else {
+    res.status(500).json({
+      message: "The user information could not be retrieved"
+  })
+}
+})
+
+//GET
+
+//DELETE
+
+//PUT
+
+module.exports = server;
+// EXPORT YOUR SERVER instead of {}
