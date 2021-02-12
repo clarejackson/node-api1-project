c75ae1b28ebe9de9993bdfcbb42586ee5a6c52fa
Clare Jackson <clare-jackson@lambdastudents.com>
2021-02-12 00:01:50 -0500

    Only one id, the put is working.

--- api/server.js
+++ api/server.js
@@ -13,39 +13,117 @@ server.get("/", (req, res) => {
 //POST 
 server.post("/api/users", (req, res) => {
   const newUser = db.insert({
+    id: db.shortid.generate(),
     name: req.body.name,
     bio: req.body.bio
   })
-  if (!req.body.name || !req.body.bio){
-    res.status(400).json({
+  newUser
+  .then(() => {
+    if (!req.body.name || !req.body.bio){
+     return res.status(400).json({
       message: "Please provide name and bio for the user",
-    })
-  } else if (!newUser){
-    res.status(500).json({
-      message: "There was an error while saving the user to the database"
-    })
+    })  
   } else {
     res.status(201).json(newUser)
   }
+})
+    .catch((error) => {
+      console.log(error)
+      res.status(500).json({
+        message: "There was an error while saving the user to the database"
+      })
+    })
+  
 })
 
 //GET
 server.get("/api/users", (req, res) => {
   const users = db.find()
-  if (users) {
+  users
+  .then((users) => {
     res.json(users)
-  } else {
+  })
+  .catch((error) => {
+    console.log(error)
     res.status(500).json({
       message: "The user information could not be retrieved"
   })
-}
-})
+  })
+});
 
 //GET
+server.get("/api/users/:id", (req, res) => {
+  const users = db.findById(req.params.id)
+  users
+  .then((users) => {
+    if (req.params.id){
+      res.json(users)
+    } else {
+      res.status(404).json({
+      message: "The user with the specified ID does not exist"
+    })
+    }
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).json({
+      message: "The user information could not be retrieved"
+    })
+  })
+})
 
 //DELETE
+server.delete("/api/users/:id", (req, res) => {
+  const users = db.findById(req.params.id)
+  users
+  .then((user) => {
+    if(req.params.id){
+      res.json(user)
+    } else {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist"
+      })
+    }
+  })
+  .catch((error) => {
+    console.log(error)
+    res.status(500).json({
+      message: "The user could not be removed"
+    })
+  })
+})
 
 //PUT
+server.put("/api/users/:id", async (req, res) => {
+  const users = await db.find()
 
-module.exports = server;
+  if (!req.params.id) {
+    return res.status(404).json({
+      message: "The user with the specified ID does not exist"
+    })
+  } else if (!req.body.name || !req.body.bio){
+    return res.status(400).json({
+      message: "Please provide name and bio for the user",
+    })  
+  }
+  // console.log(users)
+  db.update(users[req.params.id-1].id, {
+    name: req.body.name,
+    bio: req.body.bio,
+  })
+  .then((updatedUser) => {
+    console.log(updatedUser)
+      res.status(200).json(updatedUser)
+    })
+  .catch((error) => {
+    console.log(error)
+    res.status(500).json({
+      message: "The user information could not be modified"
+    })
+  })
+  })
+  
+
+module.exports = server
+
 // EXPORT YOUR SERVER instead of {}
