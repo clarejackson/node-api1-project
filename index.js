582821bf42873487a70a0ec7d81a094ebdac6ec0
Clare Jackson <clare-jackson@lambdastudents.com>
2021-02-18 22:22:57 -0500

    more updates

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const server = require('./api/server');
 const port = process.env.PORT || 5000;
 
 // START YOUR SERVER HERE
-server.listen(PORT, () => {
-  console.log(`server started at localhost:${PORT}`)
+server.listen(port, () => {
+  console.log(`server started at localhost:${port}`)
 })
